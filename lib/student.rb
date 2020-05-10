c1d517aa34664403eb3e0130460a099cb305b0a7
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-05-10 23:40:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
   end
 
   def self.create_table
-
+    DB = {:conn => SQLite3::Database.new("db/students.db")}
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
